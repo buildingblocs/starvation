a211142212a4e14a078292bb79a8adaf5cee7629
jedlimlx <jedlimlx@outlook.com>
2023-02-13 14:45:10 +0800

    Change theme to BBCS orange and make some other small changes

---
+++ src/views/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import api from "@/api/github";
+import {Repo} from "@/types/github";
+
+export default Vue.extend({
+  name: "Profile",
+  data: function () {
+    return {
+      name: "",
+      repos: Array<Repo>(),
+      loadingRepos: false,
+      headers: [{text: "Repo Name", value: "name"},
+        {text: "Language", value: "language"},
+        {text: "Description", value: "description", sortable: false},
+        {text: "Stars", value: "stargazers_count"},
+        {text: "Watchers", value: "watchers_count"}]
+    };
+  },
+  methods: {
+    async search() {
+      this.repos = await api.main(this.name);
+    }
+  }
+});
+
+</script>
