2628933d275337bcfebd9d542c0c8782ba5290e9
eli5553 <113231577+eli5553@users.noreply.github.com>
2023-03-06 22:42:43 +0800

    Update Main.vue

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,48 +1,124 @@
 <template>
   <v-flex>
   <v-container fluid>
-    <v-card>
-      <v-card-title class="pa-8" style="word-break: normal;">
-        {{ content }}
+    <ChartComponent/>
+    <v-card class="pa-8" style="word-break: normal;">
+      <v-card-title>
+        Hello, {{ user.name }}
       </v-card-title>
-      <div class="text-right pa-8">
+    
+    <br>
+    <v-data-table
+      v-model:items-per-page="itemsPerPage"
+      :headers="headers"
+      :items="stats"
+      item-value="name"
+      class="elevation-1"
+    ></v-data-table>
+    
+    </v-card>
+    <br>
+    <v-card>
+    <MainChartExample
+                style="height: 300px; max-height: 300px; margin-top: 40px"
+              />
+    </v-card>
+    <v-card>
+      <v-card-text class="pa-8" style="word-break: normal;">
+        {{ content }}
+      </v-card-text>
+      <v-card-text class="text-right pa-8">
         ~ {{ author }}
-      </div>
+      </v-card-text>
       <v-spacer />
     </v-card>
-  <br>
-  
-  <ChartComponent/>
-  </v-container>
-  
-  </v-flex>
 
+  </v-container>
+  </v-flex>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import {getRandomQuotableQuote} from "@/api/api";
 import ChartComponent from "./ChartComponent.vue"
+import school_list from "../data/schools.json";
+import challenges from "../data/challenges.json";
+import users from "../data/users.json";
+import leaderboard from "./Leaderboard.vue"
+import MainChartExample from './ChartComponent.vue'
+import { timeAgo } from "@/util/datetime";
+import { getRandomQuotableQuote } from "@/api/api";
+
+// dashboard, profile
+
 export default Vue.extend({
   name: "Home",
-  
-  components: {
-    ChartComponent
-  },
-
+  // components: {
+  //   MainChartExample
+  // },
   data: function () {
     return {
       content: "",
-      author: ""
-    };
+      author: "",
+      challenges: challenges,
+      itemsPerPage: 4,
+      headers: [
+          {
+            text: 'Your Progress',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'BBCS March 2023', align: 'end', value: 'number' },
+        ],
+      stats: [
+        {
+          name: 'Challenges Completed',
+          number: challenges.filter(it => it.completed).length,
+        },
+        {
+          name: 'Incomplete Challenges',
+          number: challenges.filter(it => !it.completed).length,
+        },
+        {
+          name: 'Mean Score',
+          number: this.$store.state.user.score,
+        },
+        {
+          name: 'Leaderboard position',
+          number: users.sort((a, b) => {
+            return a.score === b.score ? a.name.localeCompare(b.name) : (b.score ?? 0) - (a.score ?? 0);
+          }).map((it, index) => ({
+            rank: index == 0 ? "🥇" : index == 1 ? "🥈" : index == 2 ? "🥉" : index+1,
+            name: it.name,
+            username: it.username,
+            score: it.score,
+            pfp: it.profilePicture
+          })).indexOf(this.$store.state.user)
+        }
+      ]
+    }
   },
-  
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
   async mounted() {
     const quote = await getRandomQuotableQuote();
     this.content = quote.content;
     this.author = quote.author;
-    console.log(this.content, this.author);
+    console.log(this.user);
+    // console.log(this.user);
+    // console.log(this.content, this.author);
+    // this.user.school = this.getCookie("school");
+    // this.user.about = this.getCookie("about");
   }
+  // async mounted() {
+  //   const quote = await getRandomQuotableQuote();
+  //   this.content = quote.content;
+  //   this.author = quote.author;
+  //   console.log(this.content, this.author);
+  // }
 });
 
 </script>
