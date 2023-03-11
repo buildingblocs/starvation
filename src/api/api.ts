87eff076c194b646c31cfac1af6673658a98abab
Prannaya <prannayagupta@gmail.com>
2023-03-11 15:25:18 +0800

    api-ing

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import {QuotableQuote, Quote} from "@/types/quotes";
+import axios from "axios";
 
 const random_quotable_url = "https://api.quotable.io/random";
 
@@ -10,4 +11,22 @@ export async function getQuotableQuote(): Promise<QuotableQuote> {
 export async function getRandomQuotableQuote(): Promise<Quote> {
   const quote = await getQuotableQuote();
   return new Quote(quote.author, quote.content);
+}
+
+
+export async function getResults(code: string): Promise<any> {
+  let results: any = [];
+  await axios.post("/sendCode",
+  { code })
+  .then(res => {
+    results = res.data.output;
+    console.log(results);
+  },
+  res => {
+    console.log("error");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+  return results;
 }
